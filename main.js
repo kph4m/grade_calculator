aea2e0b74e40e9e9289690946dfaa56b436595d6
Khiem Pham <khiempham841@gmail.com>
2021-11-16 19:29:49 -0800

    reset input and added keyboard functionality

--- main.js
+++ main.js
@@ -1,30 +1,87 @@
-let rowCounter = 0
+let rowCounter = 1
 let totalWeight = 0
 let totalGradeProd = 0
+let totalCredits = 0
+
+// array to store values to repopulate assignment
+let assArr = []
+
+// array to store value to repopulate grade
+let gradeArr = []
+
+// array to store value to repopulate weight
+let weightArr = []
 
 // Adds new row to table
 function addNewRow () {
-  totalCredits = 0
+  // Save prev assignments in array
+  for (let i = 1; i <= rowCounter; i++) {
+    let arrVal = document.getElementById(`ass${i}`).value
+    assArr.push(arrVal)
+  }
+
+  // Save prev grades in array
+  for (let j = 1; j <= rowCounter; j++) {
+    let gradeVal = document.getElementById(`grade${j}`).value
+    gradeArr.push(gradeVal)
+  }
+
+  // Save prev weights in array
+  for (let k = 1; k <= rowCounter; k++) {
+    let weightVal = document.getElementById(`weight${k}`).value
+    weightArr.push(weightVal)
+  }
+
   totalGradeProd = 0
   rowCounter++
   document.getElementById('grade').innerHTML = ''
   document.getElementById('letter-grade').innerHTML = ''
-  console.log(rowCounter)
   document.getElementById('wrapper').innerHTML += `<span>
   <input type="text" id="ass${String(rowCounter)}">
   <input type="number" id="grade${String(rowCounter)}">
   <input type="number" id="weight${String(rowCounter)}">
   </span>`
+
+  // After new tag has been created, repopulate previous tags
+  for (let i = 1; i <= rowCounter - 1; i++) {
+    document.getElementById(`ass${i}`).value = assArr[i - 1]
+  }
+
+  // After new tag has been created, repopulate previous tags
+  for (let j = 1; j <= rowCounter - 1; j++) {
+    document.getElementById(`grade${j}`).value = gradeArr[j - 1]
+  }
+
+  // After new tag has been created, repopulate previous tags
+  for (let k = 1; k <= rowCounter - 1; k++) {
+    document.getElementById(`weight${k}`).value = weightArr[k - 1]
+  }
+
+  // Empty ass array
+  while (assArr.length > 0) {
+    assArr.pop()
+  }
+
+  // Empty grade array
+  while (gradeArr.length > 0) {
+    gradeArr.pop()
+  }
+
+  // Empty weight array
+  while (weightArr.length > 0) {
+    weightArr.pop()
+  }
 }
 
 // Calculates numeric and letter grade
 function calculateGrade () {
-  // No assignments added
+  // No inputs
   if (
-    document.getElementById('grade1') == null &&
-    document.getElementById('weight1') == null
+    document.getElementById('grade1').value == '' &&
+    document.getElementById('weight1').value == '' &&
+    document.getElementById('ass1').value == ''
   ) {
-    alert('Please add at least one assignment')
+    alert('Please input at least one assignment')
   }
 
   // Fields are blank
@@ -45,7 +102,6 @@ function calculateGrade () {
 
       // Grade Product
       var gradeProd = 0
-
       gradeProd = grade * weight
 
       // Add to total weight
@@ -92,22 +148,71 @@ function calculateGrade () {
     }
     document.getElementById('letter-grade').innerHTML = letterGrade
   }
-}
-
-// Clear all entries in table
-function clearAll () {
   totalCredits = 0
   totalGradeProd = 0
   totalGrade = 0
-  var i = rowCounter
-  while (i > 0) {
-    document.getElementById('ass' + i).value = ''
-    document.getElementById('grade' + i).value = ''
-    document.getElementById('weight' + i).value = ''
-    i--
-  }
+}
+
+// RESET TO ORIGINAL
+function reset () {
+  location.reload()
+
+  /* Can't grab input values (becomes null) after using this method for some reason...
+  totalCredits = 0
+  totalGradeProd = 0
+  totalGrade = 0
+
   totalWeight = 0
   totalGradeProd = 0
   document.getElementById('grade').innerHTML = ''
   document.getElementById('letter-grade').innerHTML = ''
+
+  let i = 1
+  let j = 1
+  let k = 1
+
+  document.getElementById('ass1').value = ''
+  document.getElementById('grade1').value = ''
+  document.getElementById('weight1').value = ''
+
+  i++
+  j++
+  k++
+
+  // Loop through all assignment inputs and remove them
+  while (i <= rowCounter) {
+    document.getElementById('ass' + i).remove()
+    i++
+  }
+
+  // Loop through all grade inputs and remove them
+  while (j <= rowCounter) {
+    document.getElementById('grade' + j).remove()
+    j++
+  }
+
+  // Loop through all weight inputs and remove them
+  while (k <= rowCounter) {
+    document.getElementById('weight' + k).remove()
+    k++
+  }
+  */
 }
+
+// KEYBOARD FUNCTIONALITY
+document.addEventListener('keyup', e => {
+  // If plus, call addNewRow
+  if (e.key === 'Control') {
+    addNewRow()
+  }
+
+  // If enter, call calculateGrade
+  if (e.key === 'Enter') {
+    calculateGrade()
+  }
+
+  // If esc, call reset
+  if (e.key === 'Escape') {
+    reset()
+  }
+})
